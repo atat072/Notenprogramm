3070ec12bdb6b59678aa54829be8f6285e1088fe
atat072 <paulhenke46@gmail.com>
2021-11-10 16:07:53 +0100

    Created all Calculator functions and check for Gradingsystem

--- core/src/de/atat072/notenprogramm/main/Calculator.java
+++ core/src/de/atat072/notenprogramm/main/Calculator.java
@@ -2,11 +2,38 @@ package de.atat072.notenprogramm.main;
 
 public class Calculator {
 
-    public void Calculator(){
+    public  Calculator(){
 
     }
+    public int average(int[] smallGrades, int bigGrade, boolean level){
+        if(level){
+            return average_high(smallGrades,bigGrade);
+        } else {
+            return average_low(smallGrades, bigGrade);
+        }
+    }
 
-    public int avarage(){
+    public int total(int[] averages, boolean level){
+        if(level){
+            return total_high(averages);
+        }else {
+            return total_low(averages);
+        }
+    }
 
+    private int average_low(int[] smallGrades, int bigGrade){
+        return 0;
+    }
+
+    private int average_high(int[] smallGrades, int bigGrade){
+        return 0;
+    }
+
+    private int total_low(int[] averages){
+        return 0;
+    }
+
+    private int total_high(int[] averages){
+        return 0;
     }
 }
